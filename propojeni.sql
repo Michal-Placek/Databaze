2b4ce906b4272546a8701d61fce857196ce4a20d
Michal-Placek <michal.placek@student.ossp.cz>
2020-11-18 17:01:51 +0100

    Create propojeni.sql

---
+++ propojeni.sql
@@ -0,0 +1,15 @@
+UPDATE employees
+SET id_car = 1
+WHERE id_emloyee = 2;
+UPDATE employees
+SET id_car = 2
+WHERE id_emloyee = 1;
+UPDATE employees
+SET id_car = 3
+WHERE id_emloyee = 4;
+UPDATE employees
+SET id_car = 4
+WHERE id_emloyee = 3;
+UPDATE employees
+SET id_car = 5
+WHERE id_emloyee = 5;
