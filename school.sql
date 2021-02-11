25573ea5e8b6a8286ab3027e7240f41558c9a000
Michal-Placek <michal.placek@student.ossp.cz>
2021-02-11 08:45:27 +0100

    Update school.sql

--- school.sql
+++ school.sql
@@ -48,3 +48,64 @@ INSERT INTO students (firstname,lastname,description,email)
 VALUES ('Klára','Paulová','Number3','klara.paulova@ossp.cz');
 INSERT INTO students (firstname,lastname,description,email)
 VALUES ('Lukáš','Jakoubek','Number4','lukas.jakoubek@ossp.cz');
+
+CREATE TABLE subjects (
+    id_subject int AUTO_INCREMENT,
+    subjectname varchar(255) NOT NULL,
+    description text,
+);
+
+INSERT INTO subjects (id_subject, subjectname, description)
+VALUES ('1','Databáze','Nejlepší přemět');
+INSERT INTO subjects (id_subject, subjectname, description)
+VALUES ('2','Algoritmizace','Skvělý předmět');
+INSERT INTO subjects (id_subject, subjectname, description)
+VALUES ('3','Programové vybavení','Dobrý předmět');
+INSERT INTO subjects (id_subject, subjectname, description)
+VALUES ('4','Matematika','Předmět kde se počítají jablka');
+INSERT INTO subjects (id_subject, subjectname, description)
+VALUES ('5','Český jazyk','Neumím rodný jazyk');
+INSERT INTO subjects (id_subject, subjectname, description)
+VALUES ('6','Anglický jazyk','Ale za to umím cizí jazyk');
+INSERT INTO subjects (id_subject, subjectname, description)
+VALUES ('7','Fyzika','Jablko spadlo rychlostí...');
+INSERT INTO subjects (id_subject, subjectname, description)
+VALUES ('8','Chemie','BUM');
+INSERT INTO subjects (id_subject, subjectname, description)
+VALUES ('9','Komunikace','Dobrý den');
+INSERT INTO subjects (id_subject, subjectname, description)
+VALUES ('10','Značkovací jazyky','Python je na prd, change my mind');
+
+ALTER TABLE subjects
+ADD shortname varchar(10);
+UPDATE subjects
+
+SET shortname = 'Da'
+WHERE id_subjects = 1;
+UPDATE subjects
+SET shortname = 'Ag'
+WHERE id_subjects = 2;
+UPDATE subjects
+SET shortname = 'Pv'
+WHERE id_subjects = 3;
+UPDATE subjects
+SET shortname = 'M'
+WHERE id_subjects = 4;
+UPDATE subjects
+SET shortname = 'Čj'
+WHERE id_subjects = 5;
+UPDATE subjects
+SET shortname = 'Aj'
+WHERE id_subjects = 6;
+UPDATE subjects
+SET shortname = 'Fy'
+WHERE id_subjects = 7;
+UPDATE subjects
+SET shortname = 'Ch'
+WHERE id_subjects = 8;
+UPDATE subjects
+SET shortname = 'Kom'
+WHERE id_subjects = 9;
+UPDATE subjects
+SET shortname = 'Zj'
+WHERE id_subjects = 10;
